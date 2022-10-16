d31eb93de37bef9ab4b479d7327a7be0d5e7b45a
jersonb <jersonlbrito@hotmail.com>
2022-10-16 02:19:55 -0300

    feat: add request geolocation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,31 +6,70 @@ namespace Locaserv.Bdv.App;
 
 public partial class MainPage : ContentPage
 {
-    private int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private async void OnCounterClicked(object sender, EventArgs e)
+    private async void GetComunication(object sender, EventArgs e)
     {
-        var a = await GetTets();
-
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = JsonSerializer.Serialize(a.deu_bom);
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
-
-    private static async Task<Test> GetTets()
-    {
+        Loading.IsRunning = true;
         var testApi = RestService.For<ITest>("https://locaserv-bdv-api.herokuapp.com/");
 
-        return await testApi.GetTest();
+        var result = await testApi.GetTest();
+
+        ApiComnunicationLbl.Text = JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true });
+        Loading.IsRunning = false;
+
+        SemanticScreenReader.Announce(ApiComnunicationLbl.Text);
+    }
+
+    private async void GetLocation(object sender, EventArgs e)
+    {
+        Loading.IsRunning = true;
+
+        var permissionStatus = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (permissionStatus != PermissionStatus.Granted)
+        {
+            var message = "Sua localização atual será usada apenas durante o uso do app.\n" +
+                          "Utilizaremos sua Localização facilitar seu uso.";
+
+            await DisplayAlert("Permissão de Geolocalização", message, "Ok");
+        }
+
+        permissionStatus = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+        if (permissionStatus == PermissionStatus.Granted)
+        {
+            var geolocation = await Geolocation.GetLocationAsync();
+
+            var latitude = geolocation.Latitude;
+            var longitude = geolocation.Longitude;
+
+            var placemarks = await Geocoding.GetPlacemarksAsync(latitude, longitude);
+
+            var placemark = placemarks.FirstOrDefault();
+
+            var geocodeAddress =
+                $"Rua/Logradouro:   {placemark.Thoroughfare}\n" +
+                $"Número(1):        {placemark.SubThoroughfare}\n" +
+                $"Número(2):        {placemark.FeatureName}\n" +
+                $"Bairro:           {placemark.SubLocality}\n" +
+                $"Cidade:           {placemark.SubAdminArea}\n" +
+                $"Estado:           {placemark.AdminArea}\n" +
+                $"Sigla país:       {placemark.CountryCode}\n" +
+                $"Nome País:        {placemark.CountryName}\n" +
+                $"CEP:              {placemark.PostalCode}\n";
+            GeoLocationLbl.Text = geocodeAddress;
+        }
+        else
+        {
+            GeoLocationLbl.Text = "Permissão de geolocalização Negada";
+            await DisplayAlert("Permissão de Geolocalização", "Permissão de uso da localização atual negada. \n Permita o uso da Geolocalização para o funcionamento adequado do app", "Ok");
+        }
+
+        Loading.IsRunning = false;
+        SemanticScreenReader.Announce(GeoLocationLbl.Text);
     }
 }
